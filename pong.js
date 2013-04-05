d0a1bcffdc7aba59b74f08d3f1aedd7710bf83c7
Nic Young <nryoung@gmail.com>
2013-04-04 23:21:53 -0600

    Ball now animates up and down. Collision detection for top and bottom boundaries working.

--- pong.js
+++ pong.js
@@ -1,10 +1,13 @@
+// Global variables
 var canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d'),
+    height = ctx.canvas.height,
+    width = ctx.canvas.width,
     midY = ctx.canvas.height / 2,
     midX = ctx.canvas.width / 2;
 
 
-
+//-----------------------------------------------------------------------------
 // Our ball object, actually represents the Pong to be passed around
 function Ball(x, y, w, h, fill) {
     // our constructor of sorts
@@ -14,23 +17,66 @@ function Ball(x, y, w, h, fill) {
     this.w = w;
     this.h = h;
     this.fill = fill;
+    
+    // direction of where the ball is going
+    this.vx = true;
+    this.vy = true;
 }
 
 Ball.prototype.draw = function(ctx) {
+    // Draws the ball on the canvas
     ctx.fillStyle = this.fill;
     ctx.fillRect(this.x, this.y, this.w, this.h);
 }
 
+Ball.prototype.check_collide = function(ctx) {
+    // Checks if the ball has collided with the top or bottom edges
+    // If it has it reverses the direction
+    if (this.y === height) {
+        this.vy = false;
+    } else if (this.y === 0) {
+        this.vy = true;
+    }
+}
+
+Ball.prototype.move = function(ctx) {
+    // Moves the ball depending on the direction of velocity
+    if (this.vy === true) {
+        this.y += 5;
+    } else if (this.vy === false) {
+        this.y -= 5;
+    }
+}
+
+//-----------------------------------------------------------------------------
 
 
+
+
+//-----------------------------------------------------------------------------
+// Helper functions
+function colorBackground() {
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(0, 0, width, height);
+}
+
+function animate(b) {
+    ctx.clearRect(0, 0, width, height);
+    colorBackground();
+    b.check_collide(ctx);
+    b.move(ctx);
+    b.draw(ctx);
+
+}
+//-----------------------------------------------------------------------------
 
 
 // Init function 
 function init() {
-    ctx.fillStyle = '#000000';
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    colorBackground();
     var b = new Ball(midX, midY, 5, 5, '#FFFFFF');
     b.draw(ctx);
+    setInterval(animate, 1000 / 60, b);
 }
 
 
